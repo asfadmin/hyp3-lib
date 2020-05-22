a1196435c0bab333f44866d7dee0d35f12bc09be
Joseph H Kennedy <me@jhkennedy.org>
2020-05-22 09:40:54 -0800

    [legacy-only] Change RGBA->RGB in resaple_geotiff
    On the AMIs, GDAL 1.11.3 can't handle the transparency band on pngs and
    so RGBA breaks some of the AMI processes that hit this bit of code --
    this is seen at least InSAR GAMMA.
    
    In the new containers, this doesn't seem to be an issue, so was likely
    fixed somewhere in the GDAL 1.11.3 --> 2.2.3 development, and so this
    changes shouldn't be brought into the new hyp3-lib.

--- src/resample_geotiff.py
+++ src/resample_geotiff.py
@@ -96,7 +96,7 @@ def resample_geotiff(geotiff, width, outFormat, outFile, use_nn = False):
       else:
         rgbExt = ('_rgb{0}.tif'.format(os.getpid()))
         rgbFile = outFile.replace(orgExt, rgbExt)
-        gdal.Translate(rgbFile, raster, rgbExpand='RGBA')
+        gdal.Translate(rgbFile, raster, rgbExpand='RGB')
         raster = gdal.Open(rgbFile)
         gdal.Warp(tmpFile, raster, resampleAlg=GRIORA_Cubic, width=width,
           srcNodata='0', dstSRS='EPSG:4326', dstAlpha=True)
